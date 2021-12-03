b3a2e647c70acc228d5b7e4dd9292a2d7bbb7fcd
ModernArtist <modernartist@ModernArtists-MacBook-Air.local>
2021-12-03 14:36:20 +0530

    added untracked gitignore file

--- routes/apps.js
+++ routes/apps.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const app = require('../app');
+
 
 /* GET home page. */
 router.get('/focus', function(req, res, next) {
   res.render('index', { title: 'Focus Page' });
 });
 
+router.post("/focus/create",(req,res,err)=>{
+  var data = req.body;
+  console.log(data);
+  
+  
+})
 module.exports = router;
